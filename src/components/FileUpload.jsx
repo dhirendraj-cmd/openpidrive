3640d6be8867913de73ebb5de68128e253ceed00
dhirendraj-cmd <djha5051@gmail.com>
2025-09-21 13:30:25 +0530

    file select state

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -3,10 +3,29 @@ import { useState } from "react";
 
 const FileUpload = () => {
 
+    const [selectFiles, setSelectFiles] = useState([]);
+
+    const handleFileChange = (e) => {
+        let selectedFiles = Array.from(e.target.files);
+        console.log("selectedFiles >>>>> ", selectedFiles, typeof(selectedFiles))
+
+        if (selectedFiles){
+            for (let i=0; i<=selectedFiles.length; i++){
+            
+        }
+        }
+
+        setSelectFiles(selectedFiles)
+    }
 
     return (
         <div>
-            <input type="file" multiple/>
+            <input type="file" multiple onChange={handleFileChange}/>
+            {
+                selectFiles.map((file, index) => (
+                    <li key={index}>{file.name}</li>
+                ))
+            }
             <button type="submit">Upload Files</button>
         </div>
     )
