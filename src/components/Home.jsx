4d7728146f1429865bd20bc0ca3a7f2950a5d64d
dhirendraj-cmd <djha5051@gmail.com>
2025-12-13 11:52:40 +0530

    fixed login bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,7 @@ const Home = () => {
             <div className="home-glass-box">
                 <h2>Welcome to OpenPiDrive</h2>
                 <p>
-                    OpenPiDrive is an open-source self-hosted cloud storage system powered by Raspberry Pi. 
-                    It lets you securely upload and manage personal files such as images, documents, ZIP archives, 
-                    and videos. This project is a fully open-source initiative built with simplicity and privacy in mind. You can host it at your home.
+                    No File Uploaded
                 </p>
             </div>
         </div>
