1e8bbe88c05e2e9c6d59bc2ba282c1c6af9c98a7
dhirendraj-cmd <djha5051@gmail.com>
2025-12-28 23:31:15 +0530

    removed console

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("submitting...", credentials);
+        console.log("submitting...");
         login(credentials, navigate);
     }
 
